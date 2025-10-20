7bf7a2ab23a5dae44c670a1789d7efc3cd1aaa4b
dhs <dreampath88@gmail.com>
2025-10-20 14:37:30 +0900

    done 10

--- 10-scroll/dev/app.js
+++ 10-scroll/dev/app.js
@@ -26,6 +26,7 @@ navToggle.addEventListener("click", function () {
 
 const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
+
 // ********** fixed navbar ************
 window.addEventListener("scroll", function () {
   const scrollHeight = window.pageYOffset;
@@ -45,3 +46,34 @@ window.addEventListener("scroll", function () {
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // prevent default
+    e.preventDefault();
+
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
