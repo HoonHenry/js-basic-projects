4d5f9562640fd414c8d15412c4c114dca504724d
dhs <dreampath88@gmail.com>
2025-11-07 14:36:50 +0900

    done 29

---
+++ 29-comfy-store/dev/src/displayProducts.js
@@ -0,0 +1,44 @@
+import { formatPrice } from "./utils.js";
+import { addToCart } from "./cart/setupCart.js";
+import { openCart } from "./cart/toggleCart.js";
+
+const display = (products, element, filters = false) => {
+  // display products
+  element.innerHTML = products
+    .map((product) => {
+      const { id, name, image, price } = product;
+      return `
+      <article class="product">
+        <div class="product-container">
+          <img src="${image}" alt="" class="product-img img" />
+          <!-- product icons -->
+          <div class="product-icons">
+            <a href="product.html?id=${id}" class="product-icon">
+              <i class="fas fa-search"></i>
+            </a>
+            <button class="product-cart-btn product-icon" data-id="${id}">
+              <i class="fas fa-shopping-cart"></i>
+            </button>
+          </div>
+        </div>
+        <footer>
+          <p class="product-name">${name}</p>
+          <h4 class="product-price">${formatPrice(price)}</h4>
+        </footer>
+      </article>
+    `;
+    })
+    .join("");
+
+  if (filters) return;
+
+  element.addEventListener("click", function (e) {
+    const parent = e.target.parentElement;
+    if (parent.classList.contains("product-cart-btn")) {
+      addToCart(parent.dataset.id);
+      openCart();
+    }
+  });
+};
+
+export default display;
